7d18972470bd34ce814ee1f70f2dad9e21fc0a5b
Logan Cooper <logancooper@bellsouth.net>
2021-06-08 16:05:29 -0400

    Added functionality to the script to create and populate David's card objects. Edited David's card CSS to change hover glow and card sizing. Commented HTML Header section.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 "use strict";
+
+
+
 document.addEventListener("DOMContentLoaded", function (){
     //Fetch the past launches
     FetchPastLaunches();
@@ -48,10 +51,6 @@ function FetchPastLaunchesCallback(data)
 
 function BuildPastLaunchElement(launchInfo)
 {
-    //Grab the root element on the page & create a new div for our information
-    const root = document.querySelector("#root");
-    const newLaunch = document.createElement("div");
-
     //process the info from launchInfo provided
     const missionName = launchInfo.name;
     const flightNumber = launchInfo.flight_number;
@@ -72,27 +71,61 @@ function BuildPastLaunchElement(launchInfo)
     const redditMediaLink = redditLinks.media;
     const redditRecoveryLink = redditLinks.recovery;
 
-    //create paragraph elements for each piece of information to display
-    const missionNameElement = document.createElement("p");
-    const flightNumberElement = document.createElement("p");
-    const launchSuccessElement = document.createElement("p");
+    //Create the main elements
+    const container = document.querySelector("#card-container");
+    const newLaunch = document.createElement("class");
+    newLaunch.className = "card"
+    newLaunch.setAttribute("style", "width: 18rem");
+    const newLaunchCardBody = document.createElement("div");
+    newLaunchCardBody.className = "card-body";
+    const testImageElement = document.createElement("img");
+    testImageElement.setAttribute("src", launchPatchSmall);
+    testImageElement.className = "card-img-top";
+    const newLaunchList = document.createElement("ul");
+    newLaunchList.className = "list-group list-group-flush";
+
+
+    //create children elements
+    const missionNameElement = document.createElement("h5");
+    missionNameElement.className = "card-title";
+    const flightNumberElement = document.createElement("li");
+    flightNumberElement.className = "list-group-item";
+    const launchSuccessElement = document.createElement("li");
+    launchSuccessElement.className = "list-group-item";
     const launchDetailsElement = document.createElement("p");
-    const launchDateUTCElement = document.createElement("p");
-    const articleLinkElement = document.createElement("p");
+    launchDetailsElement.className = "card-text";
+    const launchDateUTCElement = document.createElement("li");
+    launchDateUTCElement.className = "list-group-item";
+    
+
     const launchPatchSmallElement = document.createElement("p");
     const launchPatchLargeElement = document.createElement("p");
     const imagesSmallElement = document.createElement("p");
     const imagesOriginalElement = document.createElement("p");
+
+    const articleLinkElement = document.createElement("p");
     const redditLinksElement = document.createElement("p");
     const pressKitElement = document.createElement("p");
     const webCastLinkElement = document.createElement("p");
     const wikipediaLinkElement = document.createElement("p");
 
-    //Set the innertext of each paragraph element to display the data
-    missionNameElement.innerText = "Mission Name: " + missionName;
+    //Set the innertext of each element to display the data
+    missionNameElement.innerText = missionName;
     flightNumberElement.innerText = "Flight Number: " + flightNumber;
-    launchSuccessElement.innerText = "Launch Successful: " + launchSuccess;
-    launchDetailsElement.innerText = "Launch Details: " + launchDetails;
+
+    console.log(String(launchSuccess));
+    if(String(launchSuccess) === "true")
+    {
+        launchSuccessElement.innerText = "Launch Success";
+        newLaunch.className = "card success-card"
+    }
+    else if(String(launchSuccess) === "false")
+    {
+        launchSuccessElement.innerText = "Launch Failure";
+        newLaunch.className = "card failure-card"
+    }
+    
+    launchDetailsElement.innerText = "Click for flight details";
     launchDateUTCElement.innerText = "Launch Date: " + launchDateUTC;
     articleLinkElement.innerText = "Article Link: " + articleLink;
     launchPatchSmallElement.innerText = "Launch Patch (Small): " + launchPatchSmall;
@@ -105,21 +138,24 @@ function BuildPastLaunchElement(launchInfo)
     wikipediaLinkElement.innerText = "Mission Wikipedia: " + wikipediaLink;
 
     //append each element to the new launch div
-    newLaunch.append(missionNameElement);
-    newLaunch.append(flightNumberElement);
-    newLaunch.append(launchSuccessElement);
-    newLaunch.append(launchDetailsElement);
-    newLaunch.append(launchDateUTCElement);
-    newLaunch.append(articleLinkElement);
-    newLaunch.append(launchPatchSmallElement);
-    newLaunch.append(launchPatchLargeElement);
-    newLaunch.append(imagesSmallElement);
-    newLaunch.append(imagesOriginalElement);
-    newLaunch.append(redditLinksElement);
-    newLaunch.append(pressKitElement);
-    newLaunch.append(webCastLinkElement);
-    newLaunch.append(wikipediaLinkElement);
-    newLaunch.append(document.createElement("hr"))
+    newLaunchCardBody.append(missionNameElement);
+    newLaunchCardBody.append(launchDetailsElement);
+    newLaunchList.append(flightNumberElement);
+    newLaunchList.append(launchSuccessElement);
+    newLaunchList.append(launchDateUTCElement);
+    newLaunch.append(testImageElement);
+    newLaunch.append(newLaunchCardBody);
+    newLaunch.append(newLaunchList);
+    //newLaunch.append(articleLinkElement);
+    //newLaunch.append(launchPatchSmallElement);
+    //newLaunch.append(launchPatchLargeElement);
+    //newLaunch.append(imagesSmallElement);
+    //newLaunch.append(imagesOriginalElement);
+    //newLaunch.append(redditLinksElement);
+    //newLaunch.append(pressKitElement);
+    //newLaunch.append(webCastLinkElement);
+    //newLaunch.append(wikipediaLinkElement);
+    //newLaunch.append(document.createElement("hr"))
     //append the new launch div to the root
-    root.append(newLaunch);
+    container.append(newLaunch);
 }
